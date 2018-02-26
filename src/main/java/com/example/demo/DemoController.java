077833114094f8409da287224c9bd47c998efe3f
krrybarc <krystian.rybarczyk@assecobs.pl>
2018-02-26 19:39:47 +0100

    Added endpoint fetching string from config

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 @RestController
 public class DemoController {
 
+    @Value("${savedUser}")
+    private String user;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello";
@@ -18,5 +22,10 @@ public class DemoController {
     @PostMapping("/user/hello")
     public String helloUser(@RequestBody String name) {
         return "Hello " + name;
+    }
+
+    @GetMapping("/savedUser/hello")
+    public String helloSavedUser() {
+        return "Hello " + user;
     }
 }
